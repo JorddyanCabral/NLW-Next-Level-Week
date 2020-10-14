b28be6d6284893923452f8470fc8a8a08025ac38
JorddyanCabral <jorddyan.liceu@gmail.com>
2020-10-14 01:47:40 -0300

    reenvio do primeiro dia do NLW completo

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -14,7 +14,7 @@ function OrphanagesMap() {
                     <img src={mapMarkerImg} alt="Happy"/>
                     <h2> Escolha um Orfanato no mapa </h2>
                     <p>
-                        Muitas crianças estão esperando a sua visita :)
+                        Muitas crianças estão esperando a sua visita :
                     </p>
                 </header>
                 <footer>
@@ -27,7 +27,9 @@ function OrphanagesMap() {
                 zoom={13}
                 style={{width: '100%', height:'100%'}}>
                 <TileLayer url ="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                
+                {/* <TileLayer 
+                url ={`https://api.mapbox.com/syles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                /> */}
             </Map>
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#fff"/>
