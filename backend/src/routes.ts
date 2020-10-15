e02b4d574518d1ebdea58cd3476583b8029c3385
JorddyanCabral <jorddyan.liceu@gmail.com>
2020-10-15 02:30:43 -0300

    finalizando as views de orfanato

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,12 @@
 import {Router} from 'express'
+import multer from 'multer'
+
+import uploadConfig from './config/upload'
 import OrphanagesController from './controllers/OrphanagesController'
 
 const routes= Router()
+
+const upload = multer(uploadConfig)
 
 //MVC
 //Models
@@ -11,7 +16,7 @@ const routes= Router()
 
 routes.get('/orphanages', OrphanagesController.index)
 routes.get('/orphanages/:id', OrphanagesController.show)
-routes.post('/orphanages', OrphanagesController.create)
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
 
 
 export default routes;
